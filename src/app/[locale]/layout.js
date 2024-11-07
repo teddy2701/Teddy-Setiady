c7df4a028d0bad5da10b7b7fc1ff131642a8f478
Teddy Setiady <teddysetiadi850@gmail.com>
2024-11-07 19:21:04 +0700

    fixed navbar layout

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "../globals.css";
-import Navbar from "../../components/Navbar/index";
+import Navbar from "../../components/Navbar";
 import { Providers } from "../Providers";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
